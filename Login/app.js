5415b7867734ca2e980121695e3840d83c949601
chaz-cox <chazcheezecox@gmail.com>
2022-07-05 09:07:07 -0600

    front end

--- Login/app.js
+++ Login/app.js
@@ -14,7 +14,14 @@ var app = new Vue({
         data: {
             page : "Login",
             message : "",
-            name: "person",
+            threads: {},
+            thread: {},
+
+            postInput: "",
+
+            threadNameInput: "",
+            threadDescriptionInput:"",
+            threadCategoryInput:"",
 
             loginEmailInput:"",
             loginPasswordInput:"",
@@ -41,6 +48,58 @@ var app = new Vue({
                 console.log("ERROR", response.status, response);
             }
         },
+        //GetThreadsInfo
+        getThreadsInfo: async function(){
+            let response = await fetch(`${URL}/thread`,{
+            credentials: "include",
+            });
+            if (response.status == 200){
+                let body = await response.json();
+                this.threads = body;
+                console.log(this.threads);
+            }else{
+                console.log("ERROR", response.status, response);
+            }
+        },
+        gotoThreadPage: async function(id){
+            this.page = 'Thread';
+            console.log(id, " correct id");
+            let response = await fetch(`${URL}/thread/${id}`,{
+                credentials: "include",
+            });
+            if (response.status == 200){
+                let body = await response.json();
+                this.thread = body;
+                console.log(this.thread);
+            }else{
+                console.log("ERROR", response.status, response);
+            }
+        },
+
+        //submit a post
+        submitPost: async function(id){
+            console.log(id, " this is the id getting passed in");
+            let postCredentials = {body: this.postInput, thread_id: id};
+            let response = await fetch(`${URL}/post`,{
+                method: "POST",
+                body: JSON.stringify(postCredentials),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include",
+            });
+            
+            let body = await response.json();
+            console.log(body);
+            if (response.status == 201){
+                console.log("post success");
+                this.postInput = "";
+            }else{
+                console.log("ERROR:", response.status, response);
+            }
+        },
+                
+
         //POST
         postSession: async function(){
             let loginCredentials = {username: this.loginEmailInput, password: this.loginPasswordInput};
@@ -62,6 +121,7 @@ var app = new Vue({
                 this.message = "Success";
                 this.loginEmailInput = "";
                 this.loginPasswordInput = "";
+                this.getThreadsInfo()
                 this.page = 'Home';
                 
             }else if(response.status == 401){
@@ -69,6 +129,31 @@ var app = new Vue({
                 this.message = "Unsuccessful";
                 this.loginPasswordInput = "";
 
+            }else{
+                console.log("ERROR", response.status, response);
+            }
+        },
+        postThread: async function(){
+            let threadCredentials = { name: this.threadNameInput, description: this.threadDescriptionInput, category: this.threadCategoryInput};
+
+            let response = await fetch(URL + "/thread",{
+                method: "POST",
+                body: JSON.stringify(threadCredentials),
+                headers:{
+                        "Content-Type": "application/json"
+                },
+                credentials: "include"
+            });
+
+            let body = response.json();
+            console.log(body);
+
+            if (response.status == 201){
+                console.log("Success!");
+                this.threadNameInput = "";
+                this.threadDescriptionInput = "";
+                this.threadCategoryInput = "";
+                this.page = "Home";
             }else{
                 console.log("ERROR", response.status, response);
             }
